10a9330459f1b3ddfb69194841ffc5af7be70e29
Ankit Kailey <ankitkailey400@gmail.com>
2026-03-29 13:18:13 -0400

    Deployment on Render

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL", os.getenv("DATABASE_URL", "sqlite:///./students.db"))
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./students.db")
+if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
